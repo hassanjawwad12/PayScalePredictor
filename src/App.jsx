da72d60c318a52a8c6ad2cedd451c869686d0286
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2023-12-03 19:48:53 +0500

    Added output page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Landing from "./Components/Landing";
 import Login from "./Auth/Login";
 import Signup from "./Auth/Signup";
 import Predict from "./Prediction/Predict";
+import Output from "./Prediction/Output";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
             <Route path="login" element={<Login />} />
             <Route path="signup" element={<Signup />} />
             <Route path="predict" element={<Predict />} />
+            <Route path="output" element={<Output />} />
             
           </Route>
         </Routes>
---
+++ src/Prediction/Output.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Output = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen w-full bg-neutral-200'>
+      <p className='text-purple-500 text-7xl font-bold '>Your salary is in the range</p>
+    </div>
+  )
+}
+
+export default Output
